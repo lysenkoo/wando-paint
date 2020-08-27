f2ff9f81cca31ea5c3263314ab5ab311b538276b
Oleg Lysenko <lysenko@gmail.com>
2020-08-27 16:28:21 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,217 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+  app.quit();
+}
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  // Open the DevTools.
+  //mainWindow.webContents.openDevTools();
+
+  mainWindow.webContents.on('did-finish-load', ()=>{
+    let code = `  // Definitions
+    var canvas = document.getElementById("paint-canvas");
+    var context = canvas.getContext("2d");
+    var boundings = canvas.getBoundingClientRect();
+  
+    // Specifications
+    var mouseX = 0;
+    var mouseY = 0;
+    context.strokeStyle = 'black'; // initial brush color
+    context.lineWidth = 1; // initial brush width
+    var isDrawing = false;
+  
+  
+    // Handle Colors
+    var colors = document.getElementsByClassName('colors')[0];
+  
+    colors.addEventListener('click', function(event) {
+      context.strokeStyle = event.target.value || 'black';
+    });
+  
+    // Handle Brushes
+    var brushes = document.getElementsByClassName('brushes')[0];
+  
+    brushes.addEventListener('click', function(event) {
+      context.lineWidth = event.target.value || 1;
+    });
+  
+    // Mouse Down Event
+    canvas.addEventListener('mousedown', function(event) {
+      setMouseCoordinates(event);
+      isDrawing = true;
+  
+      // Start Drawing
+      context.beginPath();
+      context.moveTo(mouseX, mouseY);
+    });
+  
+    // Mouse Move Event
+    canvas.addEventListener('mousemove', function(event) {
+      setMouseCoordinates(event);
+  
+      if(isDrawing){
+        context.lineTo(mouseX, mouseY);
+        context.stroke();
+      }
+    });
+  
+    // Mouse Up Event
+    canvas.addEventListener('mouseup', function(event) {
+      setMouseCoordinates(event);
+      isDrawing = false;
+    });
+  
+    // Handle Mouse Coordinates
+    function setMouseCoordinates(event) {
+      mouseX = event.clientX - boundings.left;
+      mouseY = event.clientY - boundings.top;
+    }
+  
+    // Handle Clear Button
+    var clearButton = document.getElementById('clear');
+  
+    clearButton.addEventListener('click', function() {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+    });
+  
+    // Handle Save Button
+    var saveButton = document.getElementById('save');
+  
+    // saveButton.addEventListener('click', function() {
+    //   var imageName = prompt('Please enter image name');
+    //   var canvasDataURL = canvas.toDataURL();
+    //   var a = document.createElement('a');
+    //   a.href = canvasDataURL;
+    //   a.download = imageName || 'drawing';
+    //   a.click();
+    // });
+  `;
+    mainWindow.webContents.executeJavaScript(code);
+  });
+
+  mainWindow.on('resize', function(e){
+    e.preventDefault();
+
+    
+  })  
+  
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
+
+initPaint = function () {
+
+  // Definitions
+  var canvas = document.getElementById("paint-canvas");
+  var context = canvas.getContext("2d");
+  var boundings = canvas.getBoundingClientRect();
+
+  // Specifications
+  var mouseX = 0;
+  var mouseY = 0;
+  context.strokeStyle = 'black'; // initial brush color
+  context.lineWidth = 1; // initial brush width
+  var isDrawing = false;
+
+
+  // Handle Colors
+  var colors = document.getElementsByClassName('colors')[0];
+
+  colors.addEventListener('click', function(event) {
+    context.strokeStyle = event.target.value || 'black';
+  });
+
+  // Handle Brushes
+  var brushes = document.getElementsByClassName('brushes')[0];
+
+  brushes.addEventListener('click', function(event) {
+    context.lineWidth = event.target.value || 1;
+  });
+
+  // Mouse Down Event
+  canvas.addEventListener('mousedown', function(event) {
+    setMouseCoordinates(event);
+    isDrawing = true;
+
+    // Start Drawing
+    context.beginPath();
+    context.moveTo(mouseX, mouseY);
+  });
+
+  // Mouse Move Event
+  canvas.addEventListener('mousemove', function(event) {
+    setMouseCoordinates(event);
+
+    if(isDrawing){
+      context.lineTo(mouseX, mouseY);
+      context.stroke();
+    }
+  });
+
+  // Mouse Up Event
+  canvas.addEventListener('mouseup', function(event) {
+    setMouseCoordinates(event);
+    isDrawing = false;
+  });
+
+  // Handle Mouse Coordinates
+  function setMouseCoordinates(event) {
+    mouseX = event.clientX - boundings.left;
+    mouseY = event.clientY - boundings.top;
+  }
+
+  // Handle Clear Button
+  var clearButton = document.getElementById('clear');
+
+  clearButton.addEventListener('click', function() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  });
+
+  // Handle Save Button
+  // var saveButton = document.getElementById('save');
+
+  // saveButton.addEventListener('click', function() {
+  //   var imageName = prompt('Please enter image name');
+  //   var canvasDataURL = canvas.toDataURL();
+  //   var a = document.createElement('a');
+  //   a.href = canvasDataURL;
+  //   a.download = imageName || 'drawing';
+  //   a.click();
+  // });
+};
+
